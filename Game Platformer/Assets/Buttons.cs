c59842dfd422b55f9228f6797c0607ab8b47498b
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2021-01-08 23:30:33 +0800

    haha button go brr

---
+++ Game Platformer/Assets/Buttons.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("pressed something");
+        if (transform.parent.name == "left")
+        {
+            print("LEFT");
+            CharacterMovement.leftOrNot = true;
+            print("LEFT");
+        }
+        else if (transform.parent.name == "right")
+        {
+            print("RIGHT");
+            CharacterMovement.rightOrNot = true;
+            print("RIGHT");
+        } else 
+        {
+            return;
+        }
+    }
+    
+}
---
+++ Game Platformer/Assets/CharacterMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public static bool leftOrNot = false;
+    public static bool rightOrNot = false;
+    
+    private void Update()
+    {
+        if (leftOrNot == true) {
+            transform.position = new Vector3(transform.position.x - 5, transform.position.y, 0);
+            leftOrNot = false;
+        }
+        if (rightOrNot == true) {
+            transform.position = new Vector3(transform.position.x + 5, transform.position.y, 0);
+            rightOrNot = false;
+        }
+    }
+}
