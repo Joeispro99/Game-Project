ace6052f711a10c2e5036a71444cfa7f0097ae62
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2021-01-26 10:40:06 +0800

    invisible spike :)

--- Game Platformer/Assets/CharacterMovement.cs
+++ Game Platformer/Assets/CharacterMovement.cs
@@ -17,11 +17,21 @@ public class CharacterMovement : MonoBehaviour
     private Collider2D coll;
     private Animator anim;
     [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 7f;
     private void Start()
     {
         coll = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Spike")
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
     }
     private void Update()
     {
@@ -30,12 +40,12 @@ public class CharacterMovement : MonoBehaviour
         transform.rotation = new Quaternion((float)transform.rotation.x, (float)transform.rotation.y, 0f, (float)transform.rotation.w);
         if(hdirection < 0)
         {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            rb.velocity = new Vector2(-1*speed, rb.velocity.y);
             transform.localScale = new Vector2(-1*(float)0.3, transform.localScale.y);
         }
         else if(hdirection > 0)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
             transform.localScale = new Vector2((float)0.3, transform.localScale.y);
         } 
         else
@@ -44,12 +54,9 @@ public class CharacterMovement : MonoBehaviour
         }
         //if (Input.GetButtonDown("Jump") || Input.GetKeyDown("w") && coll.IsTouchingLayers(ground))
         //ur shitty code doesnt work
-        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        if (Input.GetButton("Jump") && coll.IsTouchingLayers(ground))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 7);
-        }
-        if (Input.GetKey("up") && coll.IsTouchingLayers(ground)){
-            rb.velocity = new Vector2(rb.velocity.y, 7);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
         if (Input.GetAxis("Fire1") > 0) 
         {
