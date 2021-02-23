bba7bd45e8ac44c5d95929600bb59ab91a168e35
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2021-02-23 11:41:44 +0800

    starry night *･゜ﾟ･*:.｡..｡.:*･'(*ﾟ▽ﾟ*)'･*:.｡. .｡.:*･゜ﾟ･*

---
+++ Game Platformer/Assets/skyFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyFollow : MonoBehaviour
+{
+    [SerializeField] private Transform characterTransform;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(characterTransform.position.x, characterTransform.position.y, 0);
+    }
+}
