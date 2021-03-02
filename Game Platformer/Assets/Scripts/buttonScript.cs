9b8edcc1a091676c1a88d41bf8603517e81b777c
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2021-03-02 09:41:12 +0800

    push

--- Game Platformer/Assets/Scripts/buttonScript.cs
+++ Game Platformer/Assets/Scripts/buttonScript.cs
@@ -10,6 +10,9 @@ public class buttonScript : MonoBehaviour
     [SerializeField] private Rigidbody2D playerrb;
     [SerializeField] private Rigidbody2D petrb;
     [SerializeField] private Rigidbody2D firerb;
+    [SerializeField] private SpriteRenderer blur;
+    [SerializeField] private Transform blurtransform;
+    [SerializeField] private Transform playertransform;
     public bool pause = false;
     
     public void OnButtonPress()
@@ -25,12 +28,15 @@ public class buttonScript : MonoBehaviour
                 playerrb.bodyType = RigidbodyType2D.Static;
                 petrb.bodyType = RigidbodyType2D.Static;
                 firerb.bodyType = RigidbodyType2D.Static;
+                blur.color = new Color(1f,1f,1f,0.5f);
+                blurtransform.position = playertransform.position;
                 pause = true;
             } else if(pause == true)
             {
                 playerrb.bodyType = RigidbodyType2D.Dynamic;
                 petrb.bodyType = RigidbodyType2D.Dynamic;
                 firerb.bodyType = RigidbodyType2D.Dynamic;
+                blur.color = new Color(1f,1f,1f,0f);
                 pause = false;
             }
         }
